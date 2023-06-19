d0ded4145f9c1bc11c52385cf5da1d93b97b7434
akzvitor <vitor_eterno@outlook.com>
2023-06-19 18:08:56 -0300

    alterando modelo Projeto

--- t08/src/main/java/com/github/akzvitor/poo/t08/Projeto.java
+++ t08/src/main/java/com/github/akzvitor/poo/t08/Projeto.java
@@ -4,12 +4,21 @@ import java.util.TreeSet;
 
 public class Projeto {
     private TreeSet linguagensDeProgramacao = new TreeSet();
+    private TreeSet pessoas = new TreeSet();
 
-    public void novaLinguagem(LinguagemDeProgramacao p) {
-        linguagensDeProgramacao.add(p);
+    public void novaLinguagem(LinguagemDeProgramacao ldp) {
+        linguagensDeProgramacao.add(ldp);
     }
 
-    public Projeto(LinguagemDeProgramacao p) {
-        linguagensDeProgramacao.add(p);
+    public Projeto(LinguagemDeProgramacao ldp) {
+        linguagensDeProgramacao.add(ldp);
+    }
+
+    public void novaPessoa(Pessoa p) {
+        pessoas.add(p);
+    }
+
+    public Projeto(Pessoa p) {
+        pessoas.add(p);
     }
 }
